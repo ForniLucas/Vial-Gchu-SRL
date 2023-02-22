5bcbe0f9cc5981b627d22acaf7671fc1d7ebaa5b
lucianagabrielavaliente <lucianagabrielavaliente@gmail.com>
2023-02-22 11:20:20 -0300

    Tabla principal de empleado

--- Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/EmpleadoDialog.java
+++ Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/EmpleadoDialog.java
@@ -5,14 +5,28 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import Domain.Empleado;
+import Controladoras.ControladorEmpleado;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.List;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
 
 public class EmpleadoDialog extends JDialog {
  
 	private final JPanel contentPanel = new JPanel();
-
+	//private JTable table;
+	ControladorEmpleado controladorEmpleado = new ControladorEmpleado();
+	
+	//Tabla Principal
+	String ids[] = {"Legajo","Apellido", "Nombre","DNI", "Teléfono","Dirección", "Fecha De Nacimiento","Estado"}; 
+	DefaultTableModel mt = new DefaultTableModel();
+	JTable table = new JTable(mt);
+	JScrollPane scrollPane = new JScrollPane(); 
 	/**
 	 * Launch the application.
 	 */
@@ -25,6 +39,7 @@ public class EmpleadoDialog extends JDialog {
 			e.printStackTrace();
 		}
 	}
+	
 
 	/**
 	 * Create the dialog.
@@ -33,11 +48,20 @@ public class EmpleadoDialog extends JDialog {
 		setBounds(50, 50, 1200, 750);
 		setResizable(false);
 		this.setTitle("GESTIÓN DE EMPLEADOS");
-		
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		
+		//Formato de tabla
+		mt.setColumnIdentifiers(ids);
+		table.setBounds(100, 40, 1000, 600);
+		scrollPane.setBounds(100, 40, 1000, 600);
+		scrollPane.setViewportView(table);
+		contentPanel.add(scrollPane);
+		//CARGAR EMPLEADOS ESTA ABAJO
+		//cargarEmpleados();
+		
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
@@ -86,5 +110,19 @@ public class EmpleadoDialog extends JDialog {
 			}
 		}
 			
+	}
+	/*
+	public void cargarEmpleados(){
+		DefaultTableModel modeloTablaEmpleado = (DefaultTableModel) table.getModel();
+		List<Empleado> filasTablaEmpleado = controladorEmpleado.listarEmpleados();
+		Iterator iterador = filasTablaEmpleado.iterator();
+		while (iterador.hasNext()) {
+			Empleado empleado = (Empleado) iterador.next();
+			String fila[] = {String.valueOf(Empleado.getId()),String.valueOf(Empleado.getApellido()),String.valueOf(Empleado.getNombre()),
+					String.valueOf(Empleado.getDni()),String.valueOf(Empleado.getTelefono()),String.valueOf(Empleado.getDireccion()),
+					String.valueOf(Empleado.FechaNac()),String.valueOf(Empleado.getDadoDeBaja())};
+			modeloTablaEmpleado.addRow(fila);
+		}
+		*/
 	}
 }
