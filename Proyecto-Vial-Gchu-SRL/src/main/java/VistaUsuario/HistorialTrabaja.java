414820379cd2f7641d839568e32ef749b077eba8
lucianagabrielavaliente <lucianagabrielavaliente@gmail.com>
2023-03-01 19:40:20 -0300

    Historial utiliza y Historial trabaja
    Cargar tablas

--- Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/HistorialTrabaja.java
+++ Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/HistorialTrabaja.java
@@ -2,19 +2,39 @@ package VistaUsuario;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import Controladoras.ControladorProyecto;
+import Domain.Trabajo;
+import Domain.Utiliza;
 
 public class HistorialTrabaja extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
+	String id = new String();
+	ControladorProyecto controladorP = new ControladorProyecto();
+	//Tabla Principal
+	String ids[] = {"DNI","Horas Trabajadas","Fecha de Inicio","Fecha Estimada de Fin","Fecha de Fin"}; 
+	DefaultTableModel mt = new DefaultTableModel();
+	JTable table = new JTable(mt);
+	JScrollPane scrollPane = new JScrollPane();
 	/**
 	 * Launch the application.
 	 */
+	/*
 	public static void main(String[] args) {
 		try {
 			HistorialTrabaja dialog = new HistorialTrabaja();
@@ -24,31 +44,61 @@ public class HistorialTrabaja extends JDialog {
 			e.printStackTrace();
 		}
 	}
-
+	*/
 	/**
 	 * Create the dialog.
 	 */
-	public HistorialTrabaja() {
-		setBounds(100, 100, 450, 300);
+	public HistorialTrabaja(ProyectoDialog dialog, String id) {
+		super(dialog, "HistorialUtiliza",true);
+		this.id = id;
+		
+		setBounds(50, 50, 600, 600);
+		this.setResizable(false);
+		this.setTitle("HISTORIAL DE EMPELADOS POR PROYECTO");
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//Formato de tabla
+		mt.setColumnIdentifiers(ids);
+		contentPanel.setLayout(null);
+		contentPanel.setLayout(null);
+		table.setBounds(89, 39, 500, 500);
+		scrollPane.setBounds(42, 55, 500, 455);
+		scrollPane.setViewportView(table);
+		contentPanel.add(scrollPane);
+		cargarTrabajo();
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		JLabel dniLbl = new JLabel("ID de proyecto" + id);
+		dniLbl.setBounds(55, 26, 200, 13);
+		contentPanel.add(dniLbl);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton cancelarBtn = new JButton("Cancelar");
+				cancelarBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelarBtn.setHorizontalAlignment(SwingConstants.RIGHT);
+				cancelarBtn.setActionCommand("Cancel");
+				buttonPane.add(cancelarBtn);
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+		}
+	}
+	//"DNI","Horas Trabajadas""Fecha de Inicio", "Fecha Estimada de Fin","Fecha de Fin" 
+	
+	public void cargarTrabajo() {
+		DefaultTableModel modeloTablaTrabaja = (DefaultTableModel) table.getModel();
+		Long idP = Long.parseLong(id);
+		Set<Trabajo> filasTrabaja = controladorP.listarTrabaja(idP);
+		Iterator<Trabajo> iterador = filasTrabaja.iterator();
+		while (iterador.hasNext()) {
+			Trabajo trabajo = (Trabajo) iterador.next();
+			String fila[] = {String.valueOf(trabajo.getEmpleado().getDni()),String.valueOf(trabajo.getHorasDeTrabajo()),String.valueOf(trabajo.getFechaInicio()),
+					String.valueOf(trabajo.getFechaEstFin()),String.valueOf(trabajo.getFechaFin())};
+			modeloTablaTrabaja.addRow(fila);
 		}
 	}
 
--- Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/HistorialUtiliza.java
+++ Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/HistorialUtiliza.java
@@ -1,20 +1,39 @@
 package VistaUsuario;
 
 import java.awt.BorderLayout;
+
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import Controladoras.ControladorProyecto;
+import Domain.Utiliza;
 
 public class HistorialUtiliza extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
+	String id = new String();
+	ControladorProyecto controladorP = new ControladorProyecto();
+	//Tabla Principal
+	String ids[] = {"Fecha de Inicio", "Fecha Estimada de Fin","Fecha de Fin", "Patente","Descripcion"}; 
+	DefaultTableModel mt = new DefaultTableModel();
+	JTable table = new JTable(mt);
+	JScrollPane scrollPane = new JScrollPane();
 	/**
 	 * Launch the application.
 	 */
+	/*
 	public static void main(String[] args) {
 		try {
 			HistorialUtiliza dialog = new HistorialUtiliza();
@@ -24,31 +43,61 @@ public class HistorialUtiliza extends JDialog {
 			e.printStackTrace();
 		}
 	}
-
+	*/
 	/**
 	 * Create the dialog.
 	 */
-	public HistorialUtiliza() {
-		setBounds(100, 100, 450, 300);
+	public HistorialUtiliza(ProyectoDialog dialog, String id) {
+		super(dialog, "HistorialUtiliza",true);
+		this.id = id;
+		
+		setBounds(50, 50, 600, 600);
+		this.setResizable(false);
+		this.setTitle("HISTORIAL DE MAQUINARIA UTILIZADA POR PROYECTO");
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		//Formato de tabla
+		mt.setColumnIdentifiers(ids);
+		contentPanel.setLayout(null);
+		contentPanel.setLayout(null);
+		table.setBounds(89, 39, 500, 500);
+		scrollPane.setBounds(42, 55, 500, 455);
+		scrollPane.setViewportView(table);
+		contentPanel.add(scrollPane);
+		cargarUtiliza();
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		JLabel dniLbl = new JLabel("ID de proyecto" + id);
+		dniLbl.setBounds(55, 26, 200, 13);
+		contentPanel.add(dniLbl);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton cancelarBtn = new JButton("Cancelar");
+				cancelarBtn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelarBtn.setHorizontalAlignment(SwingConstants.RIGHT);
+				cancelarBtn.setActionCommand("Cancel");
+				buttonPane.add(cancelarBtn);
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+		}
+	}
+	//"Legajo","Fecha de Inicio", "Fecha Estimada de Fin","Fecha de Fin" "Maquinaria"
+	
+	public void cargarUtiliza() {
+		DefaultTableModel modeloTablaUtiliza = (DefaultTableModel) table.getModel();
+		Long idP = Long.parseLong(id);
+		Set<Utiliza> filasUtiliza = controladorP.listarUtiliza(idP);
+		Iterator<Utiliza> iterador = filasUtiliza.iterator();
+		while (iterador.hasNext()) {
+			Utiliza utiliza = (Utiliza) iterador.next();
+			String fila[] = {String.valueOf(utiliza.getFechaInicio()),String.valueOf(utiliza.getFechaEstFin()),
+					String.valueOf(utiliza.getFechaFin()),String.valueOf(utiliza.getMaquinaria().getCodigo()),String.valueOf(utiliza.getMaquinaria().getDescripcion())};
+			modeloTablaUtiliza.addRow(fila);
 		}
 	}
 
