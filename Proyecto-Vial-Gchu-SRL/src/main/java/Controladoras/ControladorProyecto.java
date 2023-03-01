2c8f7d47ecf52ff70a896fa3adc197b59ead588a
Lucas Forni <70183535+ForniLucas@users.noreply.github.com>
2023-03-01 17:09:40 -0300

    AsignarTipoProyecto en controlador proyecto

--- Proyecto-Vial-Gchu-SRL/src/main/java/Controladoras/ControladorProyecto.java
+++ Proyecto-Vial-Gchu-SRL/src/main/java/Controladoras/ControladorProyecto.java
@@ -223,6 +223,44 @@ public void finalizarProyecto(Proyecto unProyecto, LocalDate unaFechaFin) {
 	    	  StandardServiceRegistryBuilder.destroy(registry);
 	    }
 	}
+
+public void asignarTipoProyecto(Proyecto unProyecto, TipoProyecto unTipo) {
+	
+	// Iniciar sesión de Hibernate
+    StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+    SessionFactory factory = null;
+    Session session = null;
+    Transaction transaction = null;
+    try {
+      factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+      session = factory.openSession();
+      transaction = session.beginTransaction();
+      unProyecto.asignarTipoProyecto(unTipo);
+      //Actualiza el proyecto
+      session.update(unProyecto);
+      transaction.commit();
+      
+      //Actualizar el proyecto en la lista
+      actualizarlistado(unProyecto);
+      
+    } catch (Exception ex) {
+    	  // Realizar un rollback en caso de una excepción
+    	  if (transaction != null) {
+    	    transaction.rollback();
+    	  }
+    	  System.out.println(ex.getMessage());
+    	  ex.printStackTrace();
+    	} finally {
+    	  // Cerrar la sesión y destruir el registro del servicio
+    	  if (session != null) {
+    	    session.close();
+    	  }
+    	  if (factory != null) {
+    	    factory.close();
+    	  }
+    	  StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
 	
 	
 	public Proyecto buscarID (int idProyecto) {
