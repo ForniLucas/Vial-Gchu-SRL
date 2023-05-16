a9f9e27d95b9cdd9978621883e3113a40ca30ec6
Martin <tincho1597lopez@gmail.com>
2023-05-16 11:16:28 -0300

    Gestion de boton en Proyecto Dialog y modificación en Alta Proyecto

--- Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/ProyectoDialog.java
+++ Proyecto-Vial-Gchu-SRL/src/main/java/VistaUsuario/ProyectoDialog.java
@@ -17,10 +17,15 @@ import Controladoras.ControladorProyecto;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class ProyectoDialog extends JDialog {
@@ -31,6 +36,7 @@ public class ProyectoDialog extends JDialog {
 	DefaultTableModel mt = new DefaultTableModel();
 	JTable table = new JTable(mt);
 	JScrollPane scrollPane = new JScrollPane(); 
+	JOptionPane optionPane = new JOptionPane();
 	ControladorProyecto controladorProyecto = new ControladorProyecto();
 	String id = new String();
 	String nombre = new String();
@@ -111,6 +117,39 @@ public class ProyectoDialog extends JDialog {
 			JButton buscarBtn = new JButton("Buscar");
 			buscarBtn.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
+					
+					try {
+					    String fechaDesdeString = buscarFechaDesde.getText();
+					    String fechaHastaString = buscarFechaHasta.getText();
+					    String idString = buscarIdTxt.getText();
+
+					    LocalDate fechaDesde = null;
+					    LocalDate fechaHasta = null;
+					    int id = 0;
+
+					    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+					    if (fechaDesdeString.isEmpty() && fechaHastaString.isEmpty() && idString.isEmpty()) {
+					    	actualizarTabla();
+					    }
+					    else {
+					    	boolean control = validarDatos(fechaDesdeString, fechaHastaString, idString);
+					    	
+					    	if (control) {
+					    		if (!idString.isEmpty()) {
+					    			id = Integer.parseInt(idString);
+					    			cargarProyecto(id);
+					    		}
+					    		else {
+					    			fechaDesde = LocalDate.parse(fechaDesdeString, formatter);
+						    		fechaHasta = LocalDate.parse(fechaHastaString, formatter);
+						    		cargarProyectos(fechaDesde,fechaHasta);
+					    		}
+					    	}
+					    }
+					}catch (Exception ex) {
+					    optionPane.showMessageDialog(null, ex.getMessage());
+					}
 				}
 			});
 			buscarBtn.setBounds(1017, 25, 85, 19);
@@ -202,7 +241,102 @@ public class ProyectoDialog extends JDialog {
 		}
 		
 	}
-	public void refrescar() {
-		table.repaint();;
+	
+	public void cargarProyectos(LocalDate desde, LocalDate hasta) {
+		DefaultTableModel modeloTablaProyecto = (DefaultTableModel) table.getModel();
+		modeloTablaProyecto.setRowCount(0);
+		List<Proyecto> filasTablaProyecto = controladorProyecto.buscarEntreFechas(desde, hasta);
+		Iterator<Proyecto> iterador = filasTablaProyecto.iterator();
+		while (iterador.hasNext()) {
+			Proyecto proyecto = (Proyecto) iterador.next();
+			String estado = proyecto.getEstado().toString();
+			String fila[] = {String.valueOf(proyecto.getId()),String.valueOf(proyecto.getNombre()),String.valueOf(proyecto.getFechaInicio()),
+					String.valueOf(proyecto.getFechaEstFin()),String.valueOf(estado),String.valueOf(proyecto.getFechaFin())};
+			modeloTablaProyecto.addRow(fila);
+		}
+	}
+	
+	public void cargarProyecto(int id) {
+		DefaultTableModel modeloTablaProyecto = (DefaultTableModel) table.getModel();
+		modeloTablaProyecto.setRowCount(0);
+		List<Proyecto> proyectoEncontrados = new LinkedList<Proyecto>();
+		Proyecto proyectoEncontrado = controladorProyecto.buscarID(id);
+		if (proyectoEncontrado != null) proyectoEncontrados.add(proyectoEncontrado);
+		List<Proyecto> filasTablaProyecto = proyectoEncontrados;
+		Iterator<Proyecto> iterador = filasTablaProyecto.iterator();
+		while (iterador.hasNext()) {
+			Proyecto proyecto = (Proyecto) iterador.next();
+			String estado = proyecto.getEstado().toString();
+			String fila[] = {String.valueOf(proyecto.getId()),String.valueOf(proyecto.getNombre()),String.valueOf(proyecto.getFechaInicio()),
+					String.valueOf(proyecto.getFechaEstFin()),String.valueOf(estado),String.valueOf(proyecto.getFechaFin())};
+			modeloTablaProyecto.addRow(fila);
+		}
+	}
+	
+	public boolean validarDatos(String fechaDesde, String fechaHasta, String id) {
+	    boolean resultado = true;
+
+	    if ((!fechaDesde.isEmpty() && !id.isEmpty()) || (!fechaHasta.isEmpty() && !id.isEmpty())) {
+	    	JOptionPane.showMessageDialog(null, "Debe elejir solo un criterio de busqueda");
+            resultado = false;
+            return resultado;
+	    }
+	    
+	    
+	    // Validar que el ID sea un número válido y mayor a 0
+	    if (!id.isEmpty()) {
+	        try {
+	            int idNum = Integer.parseInt(id);
+	            if (idNum <= 0) {
+	                JOptionPane.showMessageDialog(null, "El ID debe ser mayor a 0.");
+	                resultado = false;
+	                return resultado;
+	            }
+	        } catch (NumberFormatException e) {
+	            JOptionPane.showMessageDialog(null, "Ingrese un valor numérico válido para el ID.");
+	            resultado = false;
+	            return resultado;
+	        }
+	    }
+
+	    // Validar las fechas
+	    if(!fechaDesde.isEmpty() && fechaHasta.isEmpty()) {
+	    	JOptionPane.showMessageDialog(null, "Debe completar fecha Hasta");
+            resultado = false;
+            return resultado;
+	    }
+	    
+	    if(fechaDesde.isEmpty() && !fechaHasta.isEmpty()) {
+	    	JOptionPane.showMessageDialog(null, "Debe completar fecha Desde");
+            resultado = false;
+            return resultado;
+	    }
+	    
+	    if (!fechaDesde.isEmpty() && !fechaHasta.isEmpty()) {
+	        try {
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	            LocalDate desde = LocalDate.parse(fechaDesde, formatter);
+	            LocalDate hasta = LocalDate.parse(fechaHasta, formatter);
+	            if (desde.isAfter(hasta)) {
+	                JOptionPane.showMessageDialog(null, "La fecha desde no puede ser posterior a la fecha hasta.");
+	                resultado = false;
+	                return resultado;
+	            }
+	        } catch (DateTimeParseException ex) {
+	            JOptionPane.showMessageDialog(null, "Ingrese fechas válidas con el formato dd/MM/yyyy.");
+	            resultado = false;
+	            return resultado;
+	        }
+	    }
+
+	    return resultado;
+	}
+
+
+	
+	public void actualizarTabla() {
+		 DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+		    modelo.setRowCount(0); // Limpia la tabla
+		    cargarProyectos(); 
 	}
 }
